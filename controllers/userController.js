e3d8a255046167a9b7131474052edfced812e922
alinebeatrizw <alinewille31@gmail.com>
2020-10-04 20:56:07 -0300

    add date register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+
+
 class UserController{
 
 
@@ -15,12 +17,20 @@ class UserController{
 
             event.preventDefault();
 
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
             let values = this.getValues();
 
             this.getPhoto().then(
                 (content)=>{
                     values.photo = content;
                     this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
             }, 
             
             (e)=>{
@@ -49,7 +59,12 @@ class UserController{
             fileReader.onerror = (e)=>{
                 reject(e);
             };
+
+            if(file){
             fileReader.readAsDataURL(file);
+            }else{  
+                resolve('dist/img/boxed-bg.jpg');
+            }
         });
 
  
@@ -64,7 +79,12 @@ class UserController{
                 if (field.checked){
                 user[field.name] = field.value;
             }
-        }else{
+        }else if(field.name == "admin"){
+
+            user[field.name] = field.checked;
+        }
+        
+        else{
             user[field.name] = field.value;
         }
     });
@@ -83,20 +103,23 @@ class UserController{
     }
  addLine(dataUser){
 
-        this.tableEl.innerHTML = `
-        
-        <tr>
+
+        let tr = document.createElement("tr");
+
+        tr.innerHTML =  `
+  
         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
-        <td>${dataUser.birth}</td>
+        <td>${(dataUser.admin)? 'Sim' : "Não"}</td>
+        <td>${Utils.dateFormat(dataUser.register)}</td>
         <td>
           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
           <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>
-      </tr>
         `;
+
+        this.tableEl.appendChild(tr);
     
     }
 }
